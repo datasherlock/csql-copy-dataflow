2208f31c5f833b6501c3da0f3aafe93b87ff332f
Jerome <contact@jeromerajan.com>
2024-08-20 14:39:45 +0530

    Added new configs for source file

--- sinks/write_to_cloudsql_pgcopy.py
+++ sinks/write_to_cloudsql_pgcopy.py
@@ -1,6 +1,6 @@
 import logging
 from datetime import datetime
-
+from sources.csv import CSV
 import apache_beam as beam
 from google.cloud import storage
 from io import StringIO
@@ -16,6 +16,9 @@ class CopyCsvToPostgres(beam.DoFn):
         self.connection = None
         self.config = config
         self.table_name = config.get_config("cloudsql", "table_name")
+        self.delimiter = config.get_config("source", "delimiter")
+        self.header = config.get_config("source", "header")
+        self.columns = config.get_config("source", "column_list")
         self.logger = Logger().get_logger()
 
     def start_bundle(self):
@@ -26,13 +29,13 @@ class CopyCsvToPostgres(beam.DoFn):
 
     def copy_data_to_table(self, csv_buffer):
         start_time = datetime.now()  # Capture the start time
+        source_file = CSV(self.delimiter, self.header, self.columns)
         self.logger.info(f"Starting copy operation at {start_time}")
-
         connection = self.engine.raw_connection()
         cursor = connection.cursor()
         # cursor.execute(f"TRUNCATE TABLE {self.table_name};") # This hints PG to not use WAL
         cursor.copy_expert(f"""
-            COPY {self.table_name} FROM STDIN WITH (FORMAT CSV, DELIMITER E'{chr(29)}', HEADER FALSE)
+            COPY {self.table_name} ({source_file.columns}) FROM STDIN WITH (FORMAT CSV, DELIMITER E'{chr(source_file.delimiter)}', HEADER {source_file.header})
         """, csv_buffer)
         connection.commit()
         cursor.close()
@@ -50,7 +53,10 @@ class CopyCsvToPostgres(beam.DoFn):
         blob = bucket.blob(file_path)
         file_content = blob.download_as_text()
 
-        # Load CSV data into a StringIO buffer with ASCII 29 delimiter
+        # The cursor.copy_expert method in the psycopg2 library allows you to use custom SQL commands with the COPY
+        # operation. When using COPY FROM STDIN, PostgreSQL expects the input data to be streamed as if it were being
+        # read from a standard input or a file. StringIO is an in-memory stream that behaves like a file object and
+        # establishes compatibility with psycopg2 copy_expert
         buffer = StringIO()
         buffer.write(file_content)
         buffer.seek(0)
